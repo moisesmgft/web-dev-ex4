9e0b4bbc37b4487d56dc1987c1d299af0634d83d
moisesmgft <moisesmgft@gmail.com>
2023-09-18 22:45:25 -0300

    'POST' funcionando

--- app.js
+++ app.js
@@ -1,41 +1,72 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
+const fs = require('fs'); // Importe o módulo fs
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-// Verificação de extensão personalizada
-function verificaExtensao(req, file, cb) {
-  const extensoesPermitidas = ['json'];
-  const extensao = file.originalname.split('.').pop();
-  
-  if (extensoesPermitidas.includes(extensao)) {
-    cb(null, true); // Extensão permitida
-  } else {
-    cb(new Error('Erro: O arquivo enviado não é um arquivo JSON válido.'), false); // Extensão não permitida
-  }
+// Função para excluir todos os arquivos na pasta /uploads
+function excluirArquivosUploads() {
+  const pastaUploads = path.join(__dirname, 'uploads');
+
+  fs.readdir(pastaUploads, (err, arquivos) => {
+    if (err) {
+      console.error('Erro ao listar arquivos na pasta /uploads:', err);
+      return;
+    }
+
+    for (const arquivo of arquivos) {
+      const caminhoArquivo = path.join(pastaUploads, arquivo);
+
+      fs.unlink(caminhoArquivo, (err) => {
+        if (err) {
+          console.error('Erro ao excluir arquivo:', caminhoArquivo, err);
+        } else {
+          console.log('Arquivo excluído:', caminhoArquivo);
+        }
+      });
+    }
+  });
 }
 
-// Configuração do Multer com a verificação de extensão
-const upload = multer({
-  storage: multer.memoryStorage(),
-  fileFilter: verificaExtensao,
+// Exclua os arquivos na pasta /uploads durante a inicialização do servidor
+excluirArquivosUploads();
+
+// Configuração do Multer para o destino do upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
-app.get('/', (req, res) => {
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.json') {
+      req.fileValidationError = 'Erro: O arquivo enviado não é um arquivo JSON válido.';
+      return cb(null, false);
+    }
+    cb(null, true);
+  },
+});
+
+app.get('/formulario', (req, res) => {
   res.sendFile(__dirname + '/public/formulario.html');
 });
 
 app.post('/upload-arquivo', upload.single('arquivo'), (req, res) => {
-    if (req.file) {
-      // Em caso de sucesso, envie uma mensagem de sucesso como resposta JSON
-      res.json({ success: true, message: 'Sucesso: O arquivo JSON foi enviado com sucesso.' });
-    } else {
-      // Em caso de erro, envie uma mensagem de erro como resposta JSON
-      res.status(400).json({ success: false, message: 'Erro: O arquivo enviado não é um arquivo JSON válido.' });
-    }
-  });
+  if (!req.file) {
+    return res.status(400).json({ success: false, message: req.fileValidationError || 'Erro: Nenhum arquivo foi enviado.' });
+  }
+
+  res.status(200).json({ success: true, message: 'Sucesso: O arquivo JSON foi enviado com sucesso.' });
+});
 
 app.listen(port, () => {
   console.log(`Servidor está rodando na porta ${port}`);
